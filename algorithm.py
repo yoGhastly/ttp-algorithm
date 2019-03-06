44d2ff90c77bb9b66bfd44469c0f2d86ca9f032a
mswedrowski <swedrowskimichalms@gmail.com>
2019-03-06 16:30:44 +0100

    Calculate velocity & time function

--- algorithm.py
+++ algorithm.py
@@ -14,23 +14,31 @@ index_array = np.zeros(shape=nodes.__len__(), dtype=int)
 for x in range(nodes.__len__()):
     index_array[x] = (nodes[x][0])
 
-# Calculate distance - atm I'm assuming that we can move only on edges |xi - xj| + |yi - yj|
+# Calculate distance - pythagoras
 
 distance_array = np.zeros(shape=[nodes.__len__(), nodes.__len__()])
 
 for x in range(nodes.__len__()):
-    distance_array[x] = np.abs(nodes.transpose()[1] - nodes[x][1]) + np.abs(nodes.transpose()[2] - nodes[x][2])
+    distance_array[x] = np.power(np.power(nodes.transpose()[1] - nodes[x][1],2) + np.power(nodes.transpose()[2] - nodes[x][2],2),1/2)
 
 
 # Generate random TSP solution
 
 def generate_TSP_solution():
     index_pool = list(np.copy(index_array))
-    solution = np.empty(shape=index_array.__len__())
+    route = np.empty(shape=index_array.__len__())
     for x in range (index_array.__len__()):
         node_index = random.choice(index_pool)
-        solution[x]=node_index
+        route[x]=node_index
         index_pool.remove(node_index)
-    return solution
+    return [route,0]  # returning [route, current KPN weight]
 
+
+# Calculate velocity
+def get_velocity(solution):
+    return basic_info["max speed"] - solution[1]*(basic_info["max speed" ]- basic_info["min speed"]/basic_info["KNP cap"])
+
+# Calculate time - f(x) function
+def get_travel_time(start_index,destination_index,solution):
+        return distance_array[start_index][destination_index]/get_velocity(solution)
 
